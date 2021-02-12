d283fe5767b60477b4f6745f95ddea135f3841ff
yoyo929 <275813514@qq.com>
2021-02-12 20:57:51 +0800

    tuple

--- lesson4/for.py
+++ lesson4/for.py
@@ -1,20 +1,77 @@
-majicians=['alice','david','carolina']
-for majician in majicians:
-    print(F"{majician.title()},that was a great trick!")
-    print(f"I can't wait to see your next trick,{majician.title()}.\n")
+# majicians=['alice','david','carolina']
+# for majician in majicians:
+#     print(F"{majician.title()},that was a great trick!")
+#     print(f"I can't wait to see your next trick,{majician.title()}.\n")
 
-majicians = ['alice', 'david', 'carolina']
-for majician in majicians:
-    print(f'{majician.title()}, that was a greate trick!')
-    print(f"I can't wait to see your next trick, {majician.title()}")
-    print()
+# majicians = ['alice', 'david', 'carolina']
+# for majician in majicians:
+#     print(f'{majician.title()}, that was a greate trick!')
+#     print(f"I can't wait to see your next trick, {majician.title()}")
+#     print()
 
-majicians = ['alice', 'david', 'carolina']
-for majician in majicians:
-    print(f'{majician.title()}, that was a greate trick!')
-    print()
-    print(f"I can't wait to see your next trick, {majician.title()}")
+# majicians = ['alice', 'david', 'carolina']
+# for majician in majicians:
+#     print(f'{majician.title()}, that was a greate trick!')
+#     print()
+#     print(f"I can't wait to see your next trick, {majician.title()}")
 
+# 4.2.1
+# for majician in majicians:
+# print(majician)
+# print()
+
+# SyntaxError 语法错误
+# IndentationError 缩进错误 1.少了缩进
+# IndexError 索引越界/超出范围
+# NameError 变量在使用之前没有定义（通常也可能是变量打错字了）
+# TypeError 类型错误
+
+#nami = 12 #定义一个整型变量叫name，并赋值为12
+# name = 12
+# print(name)
+
+# 4.2.2
+# 不太可能犯的错误，想清楚循环体内容就可以了
+
+# 4.2.3
+# name = 12
+# print(name)
+
+# 4.2.4
+# 跟4.2.2相反，想清楚循环体内容就可以了
+
+# 4.2.5
+# for m in majicians # SyntaxError
+#     print(m)
 
 # / 斜杆，除法 2/3
 # \ 反斜杠，通常用于转义 n
+
+# 4.3.3
+# min max sum
+#print(max(majicians))
+
+# 4.3.2
+# r = range(0, 1_000_000, 1000)
+# a_big_list = list(r)
+# print(min(a_big_list))
+
+# 4.3.4
+# 用一个列表/范围构造一个新列表
+# squares = []
+
+# for i in range(1, 6):
+#     squares.append(i ** 2)
+
+# [1, 2, 3, 4, 5] -> [1, 4, 9, 16, 25]
+# squares = [i ** 2 for i in range(1, 6)] # [1, 2, 3, 4, 5]
+
+# print(squares)
+
+a = ['a', 'b', 'c']
+#b = ['aa', 'bb', 'cc']
+b = [i*2 for i in a]
+print(b)
+
+
+print('\n'.join([''.join([('GTYYX/'[(x-y)%len('GTYYX/')]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range(-30,30)])for y in range(15,-15,-1)]))
--- lesson4/range.py
+++ lesson4/range.py
@@ -9,24 +9,91 @@
 #     squares.append(square)
 # print(squares)
 
-digits=[1,2,3,4,5,6,7,8,9,0]
-print(min(digits))
-print(max(digits))
-print(sum(digits))
-a=len(digits)
+# digits=[1,2,3,4,5,6,7,8,9,0]
+# print(min(digits))
+# print(max(digits))
+# print(sum(digits))
+# a=len(digits)
+# print(a)
+
+# def avg(a):
+#     return sum(a) / len(a)
+
+# digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# print(avg(digits))
+
+
+
+# def multiply(a, b):
+#     m=a*b
+#     return m
+
+# m = multiply(2, 4)
+# print(m)
+
+# # 列表 -公式-> 另外一个列表
+
+# a = [1, 2, 3, 4]
+# b = [x+3 for x in a]
+# # y = f(x) = x + 3
+# print(b)
+
+# 4.4.1 切片
+
+# a = [3, 6, 8, 1, 7, 2]
+
+# b = a[:] # [2,5)
+# # [8, 1, 7]
+
+# print(b)
+
+# 思考快与慢（从第44页到第135页）
+
+# a = [3, 6, 8, 1, 7, 2]
+# b = a[:]
+
+# print(a)
+# print(b)
+
+# a[1]=5
+# print(a)
+# print(b)
+
+# b[2]=9
+# print(a)
+# print(b)
+
+
+# 4.5 元组
+
+# 列表是可变的
+# a = [3, 6, 8, 1, 7, 2, 9]
+# #del a[2]
+# print(a)
+
+# 元组是不可变的 tuple
+a = (3, 6, 8, 1, 7, 2, 9)
+# a[0] = 12
+# del a[1]
+for i in a:
+    print(i)
+
+a = 12
+
+a = 12
+a = [121, 131]
+del a[0]
 print(a)
 
-def avg(a):
-    return sum(a) / len(a)
+# genders = ('男', '女', '其他')
+# genders.append(12)
 
-digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-print(avg(digits))
+excel1 
+excel2
 
+a = [1, 2, 3, 4, 5]
+b = [3, 4, 5]
 
+for i in b:
+    a.remove(i)
 
-def multiply(a, b):
-    m=a*b
-    return m
-
-m = multiply(2, 4)
-print(m)
